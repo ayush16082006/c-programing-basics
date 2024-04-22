cc33ac95fb1cf036fa4185425f17dfd644e50702
ayush16082006 <167245419+ayush16082006@users.noreply.github.com>
2024-04-23 00:33:42 +0530

    Create sum of m to n numbers .c

---
+++ sum of m to n numbers .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int m,n,sum=0;
+printf("enter the first number");
+scanf("%d",&m);
+printf("enter the second number");
+scanf("%d",&n);
+while ("n<=m")
+{
+sum+=n;
+n++;
+}
+printf("the sum of number from %dto%d is :%d\n",n,m,sum);
+return 0;
+}
+
