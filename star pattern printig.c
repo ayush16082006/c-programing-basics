f004294cc988acb753474cfab724c751f42703ff
ayush16082006 <167245419+ayush16082006@users.noreply.github.com>
2024-05-21 23:42:31 +0530

    Add files via upload

---
+++ star pattern printig.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5; // Number of rows
+
+    for (int i = 1; i <= n; i++) {
+        // Print stars
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
